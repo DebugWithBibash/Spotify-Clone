4bcd87d2281d63ac30e10098ff233baeb239e049
DebugWithBibash <cbibash881@gmail.com>
2026-08-21 20:34:27 -0500

    Added play functionality

--- script.js
+++ script.js
@@ -17,32 +17,86 @@ async function getSongs() {
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
         if (element.href.endsWith(".mp3")) {
-            allSongs.push(element.href.split("/songs/")[1]);
+            allSongs.push(element.href);
         }
     }
 
     return allSongs;
 }
+
 async function main() {
     let songs = await getSongs();
     console.log(songs);
 
-    let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    let songDisplay = displaySong(songs);
+    let play = playSong(songs,songDisplay);
+    pausePlay(play);
+    
+    
+    // audio.pay();
+    
+    
+    // audio.addEventListener("timeupdate", () =>{
+    //     let duration = audio.duration;
+    //     let currentTime = audio.currentTime;
+    //     console.log(duration);
+    //     console.log(currentTime)
+    // })
+    
+}
 
+function displaySong(songs){
+
+    let songUl = document.querySelector(".songList").getElementsByTagName("ul")[0];
+    
     for(song of songs){
-        songUl.innerHTML = songUl.innerHTML + `<li>${song.replaceAll("%20", " ").replaceAll("%2C", "")}</li>`;
+        let part = song.split("/");   //seperating the names using split("/")
+        console.log(part);
+        let songName = decodeURIComponent(part[part.length - 1]);    //takes URL-encoded text and turns it back into normal text.
+        console.log(songName)
+        songUl.innerHTML = songUl.innerHTML + `<li>${songName}</li>`;
+    }
+    let lis = songUl.getElementsByTagName("li");
+    
+    return lis;
+}
+
+function playSong(songs, liss){
+    let audio = new Audio();
+    
+    for (let i = 0; i < liss.length; i++){
+        liss[i].addEventListener("click", (e) => {
+            let currentSong = e.target;
+            console.log(currentSong);
+            // audio.currentTime = 0;
+            audio.pause();
+            audio.src = songs[i]
+            audio.play();
+        })
     }
 
-    //play the first song
-    var audio = new Audio(songs[0]);
-    audio.pay();
-    audio.addEventListener("timeupdate", () =>{
-        let duration = audio.duration;
-        let currentTime = audio.currentTime;
-        console.log(duration);
-        console.log(currentTime)
-    })
+    return audio;
+}
 
+function pausePlay(audio){
+    let pause = document.querySelector(".pause-play");
+    pause.addEventListener("click", () => {
+        console.log("Clicked");
+        console.log(audio.src)
+        if(audio.src){
+            console.log(audio.src)
+
+            if(audio.paused){
+                audio.play();
+            }
+    
+            else{
+                audio.pause();
+    
+            }
+        }
+    })
 }
 
 main();
+
